4af671b3b529bb0b50c06ddc9683706aec69a612
OSAi-PIX <charmageddon000@gmail.com>
2025-02-08 11:30:20 -0800

    Added UI for battery and stuff!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,20 @@
+// GameManager.cs - Manages game state, night progression, and power level
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public float powerLevel = 100f;
     public bool isGameOver = false;
+    public float powerDrainRate = 0.1f; // Baseline power drain per second
+    public Text powerText;
+    public Image powerBar;
+    public GameObject blackoutScreen;
+    public Light[] lights;
+    public AudioSource powerOutSound;
+    private bool powerOutTriggered = false;
 
     void Awake()
     {
@@ -21,9 +30,15 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        if (powerLevel <= 0 && !isGameOver)
+        if (!isGameOver)
         {
-            TriggerGameOver();
+            ReducePower(powerDrainRate * Time.deltaTime);
+            UpdatePowerUI();
+        }
+        
+        if (powerLevel <= 0 && !isGameOver && !powerOutTriggered)
+        {
+            TriggerPowerShutdown();
         }
     }
 
@@ -32,9 +47,33 @@ public class GameManager : MonoBehaviour
         powerLevel = Mathf.Max(0, powerLevel - amount);
     }
 
-    void TriggerGameOver()
+    public void TriggerPowerShutdown()
+    {
+        powerOutTriggered = true;
+        blackoutScreen.SetActive(true);
+        foreach (Light light in lights)
+        {
+            light.enabled = false;
+        }
+        if (powerOutSound != null)
+        {
+            powerOutSound.Play();
+        }
+        Invoke("TriggerGameOver", 5f); // Delay game over for dramatic effect
+    }
+
+    public void TriggerGameOver()
     {
         isGameOver = true;
         SceneManager.LoadScene("GameOver");
+    }
+
+    void UpdatePowerUI()
+    {
+        if (powerText != null)
+            powerText.text = "Power: " + Mathf.RoundToInt(powerLevel) + "%";
+
+        if (powerBar != null)
+            powerBar.fillAmount = powerLevel / 100f;
     }
 }
